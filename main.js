16c2e0fd91f27ed1f6ddf0a6c1ac22135216a3c1
Reski Damayanti <dreski080@gmail.com>
2024-12-08 00:44:53 +0700

    ride section

--- main.js
+++ main.js
@@ -58,3 +58,41 @@ ScrollReveal().reveal('.location__content .location__btn', {
 	...scrollRevealOption,
 	delay: 1500,
 });
+
+const selectCards = document.querySelectorAll('.select__card');
+selectCards[0].classList.add('show__info');
+
+const price = ['225', '455', '275', '625', '395'];
+
+const priceEl = document.getElementById('select-price');
+
+function updateSwiperImage(eventName, args) {
+	if (eventName === 'beforeTransitionStart') {
+		const index = args && args[0].realIndex;
+		priceEl.innerText = price[index];
+		selectCards.forEach(item => {
+			item.classList.remove('show__info');
+		});
+		selectCards[index].classList.add('show__info');
+	}
+}
+
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	coverflowEffect: {
+		rotate: 0,
+		depth: 500,
+		modifier: 1,
+		scale: 0.75,
+		slideShadows: false,
+		stretch: -100,
+	},
+
+	onAny(event, ...args) {
+		updateSwiperImage(event, args);
+	},
+});
